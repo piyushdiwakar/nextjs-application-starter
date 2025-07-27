d9df92c9620e095d9991b67e61cb8f7d8d3dc313
piyushdiwakar <63346667+piyushdiwakar@users.noreply.github.com>
2025-07-27 18:52:07 +0530

    Add .next/static/chunks/src_app_products_[productId]_page_tsx_a993527f._.js 1753622526596

---
+++ .next/static/chunks/src_app_products_[productId]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_676b8615._.js",
+  "static/chunks/node_modules_next_141cf534._.js"
+],
+    source: "dynamic"
+});
